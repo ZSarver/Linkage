8f4f448d4ee2d249d7b77e1e5e21d99a6961b260
Zachary Sarver <Zachary.Sarver@gmail.com>
2013-07-01 14:56:56 -0500

    Rotation
    Added code to rotate cells widdershins and anti-widdershins.

--- linkage/gameelements.py
+++ linkage/gameelements.py
@@ -3,10 +3,15 @@ class Cell:
     is actually a graph, with the exits being vertices and the paths
     being edges.
 
-    The vertices of these graphs are the integers 0 through 5. The 
-    edges are tuples. So each graph is a list of edges. Up to rotation,
-    there are 5 unique cells. They are enumerated below. Cell 0 is the
-    default."""
+    The vertices of these graphs are the integers 0 through 5. The
+    edges are also lists for simplicity. So each graph is a list of
+    edges. Up to rotation, there are 5 unique cells. They are
+    enumerated below. Cell 0 is the default.
+
+    Note that these are undirected graphs, so the edge [0,3] is the
+    same as the edge [3,0].
+
+    """
     #          0 : "Asterisk"      1 : "Asteroid"          2 : "X"        3 : "bball court"     4 : "bball"
     cells = [[(0,3),(1,4),(2,5)],[(1,2),(3,4),(5,0)],[(2,3),(4,0),(5,1)],[(0,3),(1,2),(4,5)],[(0,2),(1,4),(3,5)]]
 
@@ -15,9 +20,20 @@ class Cell:
     #   1  : "neutral"
     #   2  : "player 1"
     #   3  : "player 2"
-
     def __init__(self, cellid = 0):
         self.cell = self.cells[cellid]
+    def rotate_clockwise(self):
+        """Rotating clockwise is the same as adding 1 (mod 5) to 
+        each vertex in each edge of a cell."""
+        for i in range(len(self.cell)):
+            for j in range(2):
+                self.cell[i][j] = (self.cell[i][j]+1) % 5
+    def rotate_counterclockwise(self):
+        """Rotating counterclockwise is the same as subtracting 1 (mod 5) to
+        each vertex in each edge of a cell."""
+        for i in range(len(self.cell)):
+            for j in range(2):
+                self.cell[i][j] = (self.cell[i][j]-1) % 5
 
 class Gameboard:
     """The gameboard is basically a 9 x 17 array of cells."""
